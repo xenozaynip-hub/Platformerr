3a53223e183461699ec94abfc232caff21adc44d
xenozaynip <xenozaynip@gmail.com>
2026-03-12 20:46:39 +0100

    feat(code): added a player

--- Game.py
+++ Game.py
@@ -15,6 +15,40 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+    COLOR = (0, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+    
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+             self.direction = "left"
+             self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+             self.direction = "right"
+             self.animation_count = 0
+
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+
+    def draw(self,window):
+        pygame.draw.rect(window, self.COLOR, self.rect)
+
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
     _, _, width, height = image.get_rect()
@@ -27,15 +61,20 @@ def get_background(name):
 
     return tiles, image
 
-def draw(window,background, bg_image):
+def draw(window,background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
+
+    player.draw(window)
 
     pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Gray.png") #background picture
+
+    player = Player(100, 100, 50, 50)
+
     run = True
     while run:
         clock.tick(FPS)
@@ -45,7 +84,7 @@ def main(window):
                 run = False
                 break
 
-        draw(window, background, bg_image)
+        draw(window, background, bg_image, player)
 
     pygame.quit()
     quit()
