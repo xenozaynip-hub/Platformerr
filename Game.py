ac0be9c668d9fbee4d4eccb364e3a6815e04ed51
xenozaynip <xenozaynip@gmail.com>
2026-03-13 20:22:14 +0100

    feat(code): added x movement

--- Game.py
+++ Game.py
@@ -69,6 +69,16 @@ def draw(window,background, bg_image, player):
 
     pygame.display.update()
 
+def handle_move(player):
+   keys = pygame.key.get_pressed() 
+   player.x_vel = 0
+   if keys[pygame.K_a]:
+       player.move_left(PLAYER_VEL)
+   if keys[pygame.K_d]:
+        player.move_right(PLAYER_VEL)
+   
+       
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Gray.png") #background picture
@@ -83,7 +93,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-
+        player.loop(FPS)
+        handle_move(player)
         draw(window, background, bg_image, player)
 
     pygame.quit()
