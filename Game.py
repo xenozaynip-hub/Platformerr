780f90b0d1aff1f32167f7f3e8216948286e2432
xenozaynip <xenozaynip@gmail.com>
2026-03-21 19:46:57 +0100

    refactor(player): changed the block to a character

--- Game.py
+++ Game.py
@@ -15,9 +15,37 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
 class Player(pygame.sprite.Sprite):
     COLOR = (0, 0, 0)
     GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters","VirtualGuy",32, 32, True)
 
     def __init__(self, x, y, width, height):
         super().__init__()
@@ -52,7 +80,8 @@ class Player(pygame.sprite.Sprite):
         self.fall_count += 1
 
     def draw(self,window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
