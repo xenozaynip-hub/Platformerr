8a9b47b2e0927e798a59788585dfabe70ac52114
xenozaynip <xenozaynip@gmail.com>
2026-04-20 21:30:22 +0200

    feat(player): added animation

--- Game.py
+++ Game.py
@@ -46,6 +46,7 @@ class Player(pygame.sprite.Sprite):
     COLOR = (0, 0, 0)
     GRAVITY = 1
     SPRITES = load_sprite_sheets("MainCharacters","VirtualGuy",32, 32, True)
+    ANIMATION_DELAY = 6
 
     def __init__(self, x, y, width, height):
         super().__init__()
@@ -74,13 +75,26 @@ class Player(pygame.sprite.Sprite):
              self.animation_count = 0
 
     def loop(self, fps):
-        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY) #gravity boom
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
+
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+        
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
 
     def draw(self,window):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def get_background(name):
