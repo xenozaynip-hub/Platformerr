2b041c2bb88dfb66ce4cf3b98482d8fc97cc4240
xenozaynip <xenozaynip@gmail.com>
2026-04-26 22:43:34 +0200

    feat(blocks): made a floor out of them

--- Game.py
+++ Game.py
@@ -169,7 +169,7 @@ def main(window):
     block_size = 96
 
     player = Player(100, 100, 50, 50)
-    blocks = [Block(0, HEIGHT - block_size, block_size)]
+    floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
 
     run = True
     while run:
@@ -181,7 +181,7 @@ def main(window):
                 break
         player.loop(FPS)
         handle_move(player)
-        draw(window, background, bg_image, player, blocks)
+        draw(window, background, bg_image, player, floor)
 
     pygame.quit()
     quit()
